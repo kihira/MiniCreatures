de36e68ef6576b262996be8fd710d152c36efef3
Kihira <zoe.kihira.lee@gmail.com>
2014-02-08 15:55:32 +0000

    Foxes are now tameable!

--- src/minicreatures/common/entity/EntityFox.java
+++ src/minicreatures/common/entity/EntityFox.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.ai.*;
 import net.minecraft.entity.passive.EntityChicken;
 import net.minecraft.entity.passive.EntityTameable;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -24,13 +25,75 @@ public class EntityFox extends EntityTameable {
         this.tasks.addTask(5, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
         this.tasks.addTask(5, new EntityAILookIdle(this));
         this.targetTasks.addTask(1, new EntityAITargetNonTamed(this, EntityChicken.class, 200, false));
+        this.setTamed(false);
     }
 
     @Override
     protected void applyEntityAttributes() {
         super.applyEntityAttributes();
         this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setAttribute(10.0D);
-        this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setAttribute(0.7D);
+        this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setAttribute(0.30000001192092896D);
+    }
+
+    public boolean isAIEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean interact(EntityPlayer par1EntityPlayer) {
+        ItemStack itemstack = par1EntityPlayer.inventory.getCurrentItem();
+        if (this.isTamed()) {
+            if (itemstack != null) {
+                if (itemstack.getItem() instanceof ItemFood) {
+                    ItemFood itemfood = (ItemFood)itemstack.getItem();
+                    if (itemfood.isWolfsFavoriteMeat() && this.dataWatcher.getWatchableObjectFloat(18) < 20.0F) {
+                        if (!par1EntityPlayer.capabilities.isCreativeMode) --itemstack.stackSize;
+                        this.heal((float)itemfood.getHealAmount());
+                        if (itemstack.stackSize <= 0) par1EntityPlayer.inventory.setInventorySlotContents(par1EntityPlayer.inventory.currentItem, null);
+                        return true;
+                    }
+                }
+                /* TODO Add in collars
+                else if (itemstack.itemID == Item.dyePowder.itemID) {
+                    int i = BlockColored.getBlockFromDye(itemstack.getItemDamage());
+                    if (i != this.getCollarColor()) {
+                        this.setCollarColor(i);
+                        if (!par1EntityPlayer.capabilities.isCreativeMode && --itemstack.stackSize <= 0) par1EntityPlayer.inventory.setInventorySlotContents(par1EntityPlayer.inventory.currentItem, (ItemStack)null);
+                        return true;
+                    }
+                }
+                */
+            }
+            if (par1EntityPlayer.getCommandSenderName().equalsIgnoreCase(this.getOwnerName()) && !this.worldObj.isRemote && !this.isBreedingItem(itemstack)) {
+                this.aiSit.setSitting(!this.isSitting());
+                this.isJumping = false;
+                this.setPathToEntity(null);
+                this.setTarget(null);
+                this.setAttackTarget(null);
+            }
+        }
+        else if (itemstack != null && itemstack.itemID == Item.bone.itemID) {
+            if (!par1EntityPlayer.capabilities.isCreativeMode) --itemstack.stackSize;
+            if (itemstack.stackSize <= 0) par1EntityPlayer.inventory.setInventorySlotContents(par1EntityPlayer.inventory.currentItem, null);
+            if (!this.worldObj.isRemote) {
+                if (this.rand.nextInt(3) == 0) {
+                    this.setTamed(true);
+                    this.setPathToEntity(null);
+                    this.setAttackTarget(null);
+                    this.aiSit.setSitting(true);
+                    this.setHealth(20.0F);
+                    this.setOwner(par1EntityPlayer.getCommandSenderName());
+                    this.playTameEffect(true);
+                    this.worldObj.setEntityState(this, (byte)7);
+                }
+                else {
+                    this.playTameEffect(false);
+                    this.worldObj.setEntityState(this, (byte)6);
+                }
+            }
+            return true;
+        }
+        return super.interact(par1EntityPlayer);
     }
 
     @Override
