103680b92e017d4522e4908a99b9394680b1cf59
Kihira <zoe.kihira.lee@gmail.com>
2014-02-18 00:32:31 +0000

    Fix proxy stuff so it runs

--- src/main/java/kihira/minicreatures/MiniCreatures.java
+++ src/main/java/kihira/minicreatures/MiniCreatures.java
@@ -16,13 +16,13 @@ import kihira.minicreatures.proxy.CommonProxy;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.world.biome.BiomeGenBase;
 
-@Mod(modid = "kihira/minicreatures", name = "Mini Creatures", version = "0.0.1")
+@Mod(modid = "kihira.minicreatures", name = "Mini Creatures", version = "0.0.1")
 public class MiniCreatures {
 
-    @SidedProxy(clientSide = "minicreatures.proxy.ClientProxy", serverSide = "minicreatures.proxy.CommonProxy")
+    @SidedProxy(clientSide = "kihira.minicreatures.proxy.ClientProxy", serverSide = "kihira.minicreatures.proxy.CommonProxy")
     public static CommonProxy proxy;
 
-    @Mod.Instance(value = "kihira/minicreatures")
+    @Mod.Instance(value = "kihira.minicreatures")
     public static MiniCreatures instance;
 
     @Mod.EventHandler
