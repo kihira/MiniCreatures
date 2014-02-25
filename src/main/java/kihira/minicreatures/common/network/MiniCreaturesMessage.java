5eede513849c9e8522b8444e2cfc8b42206dee28
Kihira <zoe.kihira.lee@gmail.com>
2014-02-25 15:11:59 +0000

    Improve the syncing of entity data

--- src/main/java/kihira/minicreatures/common/network/MiniCreaturesMessage.java
+++ src/main/java/kihira/minicreatures/common/network/MiniCreaturesMessage.java
@@ -8,51 +8,59 @@ import cpw.mods.fml.relauncher.Side;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import kihira.minicreatures.MiniCreatures;
+import kihira.minicreatures.common.entity.IMiniCreature;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.network.NetHandlerPlayServer;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public abstract class MiniCreaturesMessage implements IMessage {
     public abstract IMessage onMessage(MiniCreaturesMessage message, ChannelHandlerContext ctx, Side side);
 
     public static class UpdateEntityMessage extends MiniCreaturesMessage {
         private int entityID;
-        private String partsList;
+        private ArrayList<String> partsList = new ArrayList<String>();
 
         public UpdateEntityMessage() {}
         public UpdateEntityMessage(int entityID, ArrayList<String> partsList) {
             this.entityID = entityID;
-            for (String part : partsList) {
-                this.partsList += part + "|";
-            }
+            this.partsList = partsList;
         }
         public UpdateEntityMessage(int entityID, String ... partsList) {
             this.entityID = entityID;
-            for (String part : partsList) {
-                this.partsList += part + "|";
-            }
+            Collections.addAll(this.partsList, partsList);
         }
 
         @Override
         public void fromBytes(ByteBuf buf) {
             this.entityID = buf.readInt();
-            this.partsList = ByteBufUtils.readUTF8String(buf);
+            this.partsList = new ArrayList<String>();
+            while (buf.isReadable()) {
+                try {
+                    this.partsList.add(ByteBufUtils.readUTF8String(buf));
+                } catch (Exception e) {
+                    break;
+                }
+            }
         }
 
         @Override
         public void toBytes(ByteBuf buf) {
             buf.writeInt(this.entityID);
-            ByteBufUtils.writeUTF8String(buf, this.partsList);
+            for (String part : this.partsList) {
+                ByteBufUtils.writeUTF8String(buf, part);
+            }
         }
 
         @Override
         public IMessage onMessage(MiniCreaturesMessage message, ChannelHandlerContext ctx, Side side) {
             if (side == Side.SERVER) {
                 EntityPlayer entityPlayer = ((NetHandlerPlayServer) ctx.channel().attr(NetworkRegistry.NET_HANDLER).get()).playerEntity;
-                for (String part : this.partsList.split("|")) {
+                for (String part : this.partsList) {
                     if (!Strings.isNullOrEmpty(part)) MiniCreatures.logger.info("Part: " + part);
                 }
+                ((IMiniCreature) entityPlayer.worldObj.getEntityByID(this.entityID)).setParts(this.partsList);
             }
             return null;
         }
