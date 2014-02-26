99ad8cca3413aab4aedff1ae862279a14effd679
Kihira <zoe.kihira.lee@gmail.com>
2014-02-26 20:18:22 +0000

    Add in sound registration. Currently not used

--- src/main/java/kihira/minicreatures/common/customizer/CustomizerRegistry.java
+++ src/main/java/kihira/minicreatures/common/customizer/CustomizerRegistry.java
@@ -1,19 +1,21 @@
 package kihira.minicreatures.common.customizer;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
 import kihira.minicreatures.MiniCreatures;
 import kihira.minicreatures.common.entity.IMiniCreature;
 import net.minecraft.client.model.ModelBase;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class CustomizerRegistry {
 
     private static final HashMap<String, ICustomizerPart> partList = new HashMap<String, ICustomizerPart>();
+    private static final ListMultimap<EnumSoundCategory, String> soundsList = ArrayListMultimap.create();
 
     /**
      * An instance of ICustomizerPart should be passed to here
+     * @param name A unique name for the part
      * @param part The part to register
      */
     public static void registerPart(String name, ICustomizerPart<? extends ModelBase> part) {
@@ -25,12 +27,32 @@ public class CustomizerRegistry {
         else MiniCreatures.logger.error("A customizer with the name " + name + " is already registered!", new IllegalArgumentException());
     }
 
+    /**
+     * Registers a sound for the specified category. It can be used by all entities
+     * TODO Make it so sounds are limited to specific entities?
+     * @param name This must be the name that is registered with Minecraft
+     * @param soundCategories A set of categories that the sound is valid for
+     */
+    public static void registerSound(String name, EnumSet<EnumSoundCategory> soundCategories) {
+        for (EnumSoundCategory soundCategory : soundCategories) {
+            if (!soundsList.get(soundCategory).contains(name)) soundsList.put(soundCategory, name);
+            else {
+                MiniCreatures.logger.error("A sound with the name " + name + " is already registered!", new IllegalArgumentException());
+                break;
+            }
+        }
+    }
+
     public static ArrayList<String> getValidParts(IMiniCreature miniCreature, EnumPartCategory partCategory) {
         ArrayList<String> validPartsList = new ArrayList<String>();
         for (Map.Entry<String, ICustomizerPart> part : partList.entrySet()) {
             if (part.getValue().isPartValidForEntity(miniCreature.getEntity(), partCategory)) validPartsList.add(part.getKey());
         }
         return validPartsList;
+    }
+
+    public static List<String> getSoundsForCategory(EnumSoundCategory soundCategory) {
+        return soundsList.get(soundCategory);
     }
 
     public static ICustomizerPart getPart(String name) {
---
+++ src/main/java/kihira/minicreatures/common/customizer/EnumSoundCategory.java
@@ -0,0 +1,5 @@
+package kihira.minicreatures.common.customizer;
+
+public enum EnumSoundCategory {
+    STEP, DEATH, HURT, LIVING
+}
