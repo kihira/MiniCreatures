28147700d94688ad6b31559ea8be67b1356a8570
Kihira <zoe.kihira.lee@gmail.com>
2014-03-17 17:30:56 +0000

    Mini Players will now shoot bows if they are holding a bow

--- src/main/java/kihira/minicreatures/common/entity/EntityMiniPlayer.java
+++ src/main/java/kihira/minicreatures/common/entity/EntityMiniPlayer.java
@@ -7,12 +7,14 @@ import cpw.mods.fml.relauncher.SideOnly;
 import kihira.minicreatures.MiniCreatures;
 import kihira.minicreatures.common.customizer.EnumPartCategory;
 import net.minecraft.block.Block;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.*;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.passive.EntityTameable;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.IInventory;
@@ -29,11 +31,14 @@ import net.minecraft.world.World;
 import java.util.ArrayList;
 import java.util.EnumSet;
 
-public class EntityMiniPlayer extends EntityTameable implements IMiniCreature {
+public class EntityMiniPlayer extends EntityTameable implements IMiniCreature, IRangedAttackMob {
 
     private final InventoryBasic inventory = new InventoryBasic(this.getCommandSenderName(), false, 18);
     //True if aiming with bow. Not currently in use.
     public boolean isAiming = false;
+
+    private EntityAIArrowAttack aiArrowAttack = new EntityAIArrowAttack(this, 1.0D, 20, 60, 15.0F);
+    private EntityAIAttackOnCollide aiAttackOnCollide = new EntityAIAttackOnCollide(this, 1.2D, true);
 
     //Maintain an array list client side for previewing
     @SideOnly(Side.CLIENT)
@@ -46,12 +51,14 @@ public class EntityMiniPlayer extends EntityTameable implements IMiniCreature {
         this.getNavigator().setCanSwim(true);
         this.tasks.addTask(1, new EntityAISwimming(this));
         this.tasks.addTask(2, this.aiSit);
-        this.tasks.addTask(3, new EntityAIAttackOnCollide(this, 1.0D, true));
         this.tasks.addTask(4, new EntityAIFollowOwner(this, 1.0D, 10.0F, 2.0F));
         this.tasks.addTask(5, new EntityAIWander(this, 1.0D));
         this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
         this.tasks.addTask(6, new EntityAILookIdle(this));
+        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
         this.setTamed(false);
+
+        if (par1World != null && !par1World.isRemote) this.setCombatAI();
     }
 
     public boolean isChild() {
@@ -69,8 +76,8 @@ public class EntityMiniPlayer extends EntityTameable implements IMiniCreature {
         super.applyEntityAttributes();
         this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(10.0D);
         this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.30000001192092896D);
-        this.getAttributeMap().registerAttribute(SharedMonsterAttributes.attackDamage);
-        this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(4);
+        this.getAttributeMap().registerAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(4);
+        this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(40D);
     }
 
     @Override
@@ -97,6 +104,7 @@ public class EntityMiniPlayer extends EntityTameable implements IMiniCreature {
             s += tagList.getStringTagAt(i) + ",";
         }
         this.dataWatcher.updateObject(18, s);
+        this.setCombatAI();
     }
 
     @Override
@@ -177,6 +185,13 @@ public class EntityMiniPlayer extends EntityTameable implements IMiniCreature {
     }
 
     @Override
+    public void onLivingUpdate() {
+        //Gotta do this so it doesn't get stuck shooting arrows
+        if (this.getAttackTarget() != null && this.getAttackTarget().isDead) this.setAttackTarget(null);
+        super.onLivingUpdate();
+    }
+
+    @Override
     public void setAttackTarget(EntityLivingBase attackTarget) {
         if (this.isRiding() && this.ridingEntity instanceof EntityFox) {
             EntityFox entityFox = (EntityFox) this.ridingEntity;
@@ -185,12 +200,14 @@ public class EntityMiniPlayer extends EntityTameable implements IMiniCreature {
         super.setAttackTarget(attackTarget);
     }
 
+    /*
     protected void attackEntity(Entity par1Entity, float par2) {
         if (this.attackTime <= 0 && par2 < 2.0F && par1Entity.boundingBox.maxY > this.boundingBox.minY && par1Entity.boundingBox.minY < this.boundingBox.maxY) {
             this.attackTime = 20;
             this.attackEntityAsMob(par1Entity);
         }
     }
+    */
 
     @Override
     public boolean attackEntityAsMob(Entity par1Entity) {
@@ -222,8 +239,33 @@ public class EntityMiniPlayer extends EntityTameable implements IMiniCreature {
         return flag;
     }
 
+    @Override
+    public void attackEntityWithRangedAttack(EntityLivingBase var1, float var2) {
+        EntityArrow entityarrow = new EntityArrow(this.worldObj, this, var1, 1.6F, 2);
+        int power = EnchantmentHelper.getEnchantmentLevel(Enchantment.power.effectId, this.getHeldItem());
+        int punch = EnchantmentHelper.getEnchantmentLevel(Enchantment.punch.effectId, this.getHeldItem());
+        entityarrow.setDamage((double)(var2 * 2.0F) + this.rand.nextGaussian() * 0.25D + (double)((float)this.worldObj.difficultySetting.getDifficultyId() * 0.11F));
+
+        if (power > 0) entityarrow.setDamage(entityarrow.getDamage() + (double)power * 0.5D + 0.5D);
+        if (punch > 0) entityarrow.setKnockbackStrength(punch);
+        if (EnchantmentHelper.getEnchantmentLevel(Enchantment.flame.effectId, this.getHeldItem()) > 0) entityarrow.setFire(100);
+
+        this.playSound("random.bow", 1.0F, 1.0F / (this.getRNG().nextFloat() * 0.4F + 0.8F));
+        this.worldObj.spawnEntityInWorld(entityarrow);
+    }
+
+    public void setCombatAI() {
+        this.tasks.removeTask(this.aiAttackOnCollide);
+        this.tasks.removeTask(this.aiArrowAttack);
+        ItemStack itemstack = this.getHeldItem();
+
+        if (itemstack != null && itemstack.getItem() == Items.bow) this.tasks.addTask(3, this.aiArrowAttack);
+        else this.tasks.addTask(3, this.aiAttackOnCollide);
+    }
+
     public void setCarrying(ItemStack itemStack) {
         this.setCurrentItemOrArmor(0, itemStack);
+        this.setCombatAI();
     }
 
     public ItemStack getCarrying() {
