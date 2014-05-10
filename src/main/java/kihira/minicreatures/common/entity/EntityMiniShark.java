168da3f6c2f7e39c9e4809feedae99c035131159
Kihira <zoe.kihira.lee@gmail.com>
2014-05-10 18:23:12 +0100

    The Mini Shark now swims properly and attacks

--- src/main/java/kihira/minicreatures/common/entity/EntityMiniShark.java
+++ src/main/java/kihira/minicreatures/common/entity/EntityMiniShark.java
@@ -14,30 +14,31 @@
 
 package kihira.minicreatures.common.entity;
 
-import kihira.minicreatures.common.entity.ai.EntityAIWanderSwim;
+import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.EntityAIAttackOnCollide;
-import net.minecraft.entity.ai.EntityAILookIdle;
-import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
-import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.passive.EntityWaterMob;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.MathHelper;
+import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
 
 public class EntityMiniShark extends EntityWaterMob {
+
+    private double waypointX;
+    private double waypointY;
+    private double waypointZ;
+    private EntityPlayer targetedEntity;
+    private int attackTick;
 
     public EntityMiniShark(World par1World) {
         super(par1World);
         this.setSize(0.95F, 0.95F);
         this.getNavigator().setAvoidsWater(false);
         this.getNavigator().setCanSwim(true);
-        this.tasks.addTask(1, new EntityAIAttackOnCollide(this, 1.0D, true));
-        this.tasks.addTask(2, new EntityAIWanderSwim(this, 0.7D));
-        this.tasks.addTask(3, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
-        this.tasks.addTask(3, new EntityAILookIdle(this));
-        this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
         this.renderDistanceWeight = 4D;
     }
 
@@ -49,7 +50,7 @@ public class EntityMiniShark extends EntityWaterMob {
 
     @Override
     public boolean isAIEnabled() {
-        return true;
+        return false;
     }
 
     @Override
@@ -58,79 +59,119 @@ public class EntityMiniShark extends EntityWaterMob {
         return false;
     }
 
-    //TODO Make it so sharks can swim up/down. Currently don't even if their path tells them to. Issue with PathNavigate?
+    @Override
+    public boolean attackEntityAsMob(Entity par1Entity) {
+        return par1Entity.attackEntityFrom(DamageSource.causeMobDamage(this), 3);
+    }
+
+    @SuppressWarnings("SuspiciousNameCombination")
+    protected void updateEntityActionState() {
+        if (!this.worldObj.isRemote && this.worldObj.difficultySetting == EnumDifficulty.PEACEFUL) {
+            this.setDead();
+        }
+
+        if (this.attackTick > 0) this.attackTick--;
+
+        double d0 = this.waypointX - this.posX;
+        double d1 = this.waypointY - this.posY;
+        double d2 = this.waypointZ - this.posZ;
+        double d3 = d0 * d0 + d1 * d1 + d2 * d2;
+
+        if ((d3 < 1D || d3 > 3600D) && this.rand.nextInt() > 30) {
+            this.waypointX = this.posX + (double)((this.rand.nextFloat() * 2.0F - 1.0F) * 16F);
+            this.waypointY = this.posY + (double)((this.rand.nextFloat() * 2.0F - 1.0F) * 2F);
+            this.waypointZ = this.posZ + (double)((this.rand.nextFloat() * 2.0F - 1.0F) * 16F);
+
+            if (!(this.worldObj.getBlock((int) this.waypointX, (int) this.waypointY, (int) this.waypointZ) instanceof BlockLiquid)) {
+                this.waypointX = this.posX;
+                this.waypointY = this.posY;
+                this.waypointZ = this.posZ;
+            }
+        }
+
+        d3 = (double) MathHelper.sqrt_double(d3);
+
+        if (this.isCourseTraversable(d3)) {
+            this.motionX += d0 / d3 * 0.02D;
+            this.motionY += d1 / d3 * 0.02D;
+            this.motionZ += d2 / d3 * 0.02D;
+        }
+        else {
+            this.waypointX = this.posX;
+            this.waypointY = this.posY;
+            this.waypointZ = this.posZ;
+        }
+
+        if (this.targetedEntity != null && this.targetedEntity.isDead) this.targetedEntity = null;
+        if (this.targetedEntity == null) this.targetedEntity = this.worldObj.getClosestVulnerablePlayerToEntity(this, 64D);
+        if (this.targetedEntity != null && this.targetedEntity.getDistanceSqToEntity(this) < 4096D) {
+            double d5 = this.targetedEntity.posX - this.posX;
+            double d7 = this.targetedEntity.posZ - this.posZ;
+            this.renderYawOffset = this.rotationYaw = -((float)Math.atan2(d5, d7)) * 180F / (float)Math.PI;
+
+            if (this.canEntityBeSeen(this.targetedEntity) && this.targetedEntity.isInWater()) {
+                this.waypointX = this.targetedEntity.posX;
+                this.waypointY = this.targetedEntity.posY;
+                this.waypointZ = this.targetedEntity.posZ;
+
+                double d9 = this.getDistanceSq(this.targetedEntity.posX, this.targetedEntity.boundingBox.minY, this.targetedEntity.posZ);
+                double d10 = (double)(this.width * 2.0F * this.width * 2.0F + this.targetedEntity.width);
+                if (d9 <= d10 && this.attackTick <= 20) {
+                    this.attackTick = 20;
+                    this.attackEntityAsMob(this.targetedEntity);
+                }
+            }
+        }
+        else {
+            this.renderYawOffset = this.rotationYaw = -((float)Math.atan2(this.motionX, this.motionZ)) * 180F / (float)Math.PI;
+        }
+    }
+
+    private boolean isCourseTraversable(double par7) {
+        double d4 = (this.waypointX - this.posX) / par7;
+        double d5 = (this.waypointY - this.posY) / par7;
+        double d6 = (this.waypointZ - this.posZ) / par7;
+        AxisAlignedBB axisalignedbb = this.boundingBox.copy();
+
+        for (int i = 1; (double)i < par7; ++i) {
+            axisalignedbb.offset(d4, d5, d6);
+            if (!this.worldObj.getCollidingBoundingBoxes(this, axisalignedbb).isEmpty()) return false;
+        }
+
+        return true;
+    }
+
     @Override
     public void moveEntityWithHeading(float par1, float par2) {
-        //super.moveEntityWithHeading(par1, par2);
-        double d0;
-
         if (this.isInWater()) {
-            this.moveFlying(par1, par2, this.getAIMoveSpeed() / 4F);
+            float f2 = 0.91F;
+            float f3 = 0.16277136F / (f2 * f2 * f2);
+            this.moveFlying(par1, par2, this.onGround ? 0.1F * f3 : 0.02F);
+            f2 = 0.91F;
+
             this.moveEntity(this.motionX, this.motionY, this.motionZ);
-            this.motionX *= 0.800000011920929D;
-            this.motionY *= 0.800000011920929D;
-            this.motionZ *= 0.800000011920929D;
-            this.motionY = 0.0D;
+            this.motionX *= (double)f2;
+            this.motionY *= (double)f2;
+            this.motionZ *= (double)f2;
         }
         else if (this.handleLavaMovement()) {
-            d0 = this.posY;
             this.moveFlying(par1, par2, 0.02F);
             this.moveEntity(this.motionX, this.motionY, this.motionZ);
             this.motionX *= 0.5D;
             this.motionY *= 0.5D;
             this.motionZ *= 0.5D;
-            this.motionY -= 0.02D;
-
-            if (this.isCollidedHorizontally && this.isOffsetPositionInLiquid(this.motionX, this.motionY + 0.6000000238418579D - this.posY + d0, this.motionZ)) {
-                this.motionY = 0.30000001192092896D;
-            }
         }
         else {
-            float f2 = 0.91F;
-
-            if (this.onGround) {
-                f2 = this.worldObj.getBlock(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.boundingBox.minY) - 1, MathHelper.floor_double(this.posZ)).slipperiness * 0.91F;
-            }
-
-            float f3 = 0.16277136F / (f2 * f2 * f2);
-            float f4;
-
-            if (this.onGround) {
-                f4 = this.getAIMoveSpeed() * f3;
-            }
-            else {
-                f4 = this.jumpMovementFactor;
-            }
-
-            this.moveFlying(par1, par2, f4);
-            f2 = 0.91F;
-
-            if (this.onGround) {
-                f2 = this.worldObj.getBlock(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.boundingBox.minY) - 1, MathHelper.floor_double(this.posZ)).slipperiness * 0.91F;
-            }
-
+            //this.moveFlying(par1, par2, 0.02F);
             this.moveEntity(this.motionX, this.motionY, this.motionZ);
-
-            if (this.worldObj.isRemote && (!this.worldObj.blockExists((int)this.posX, 0, (int)this.posZ) || !this.worldObj.getChunkFromBlockCoords((int)this.posX, (int)this.posZ).isChunkLoaded)) {
-                if (this.posY > 0.0D) {
-                    this.motionY = -0.1D;
-                }
-                else {
-                    this.motionY = 0.0D;
-                }
-            }
-            else {
-                this.motionY -= 0.08D;
-            }
-
-            this.motionY *= 0.9800000190734863D;
-            this.motionX *= (double)f2;
-            this.motionZ *= (double)f2;
+            this.motionX *= 0.800000011920929D;
+            this.motionY = -0.800000011920929D;
+            this.motionZ *= 0.800000011920929D;
         }
     }
 
     @Override
     public boolean isInWater() {
-        return this.worldObj.handleMaterialAcceleration(this.boundingBox.expand(0.0D, -0.6000000238418579D, 0.0D), Material.water, this);
+        return this.worldObj.handleMaterialAcceleration(this.boundingBox.expand(0.0D, -0.4000000059604645D, 0.0D).contract(0.001D, 0.001D, 0.001D), Material.water, this);
     }
 }
